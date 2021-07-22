e2f58ea3284ddc02ae3b536a9005ae319c497a6b
olga <kapuchinskaya.study@gmail.com>
2021-07-22 14:19:01 +0200

    init: start the project

---
+++ kindergarten/src/routes.js
@@ -0,0 +1,13 @@
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+
+const Routes = () => {
+
+  return (
+    <BrowserRouter>
+        </BrowserRouter>
+  );
+  
+}
+
+
+export default Routes;
