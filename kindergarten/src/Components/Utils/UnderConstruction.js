cc646899905f72a84277f6a0961835e51160d18f
VolhaKuchynskaya <kapuchinskaya.study@gmail.com>
2021-07-30 12:22:02 +0200

    feat: add reusable component for pages under construction

---
+++ kindergarten/src/Components/Utils/UnderConstruction.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReusableButton from "./ReusableButton";
+import image from "../../Resources/images/construction.png";
+
+const UnderConstruction = (props) => {
+  return (
+    <div className="under-the-construction">
+      <img src={image} alt="under construction"></img>
+      <p>Sorry :(</p>
+      <p>The section is under construction</p>
+
+      <ReusableButton
+        text="BACK"
+        path={props.path}
+        arrow="left"
+      ></ReusableButton>
+    </div>
+  );
+};
+
+export default UnderConstruction;
