d95f59beb2e820305b4a405e7f7bb6659f22b5ce
VolhaKuchynskaya <kapuchinskaya.study@gmail.com>
2021-07-30 12:31:59 +0200

    feat: add routes, styles for gallery and for-parents sections

--- kindergarten/src/Components/Home/index.js
+++ kindergarten/src/Components/Home/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Carousel from "./PhotoCarousel/Carousel";
-import ArticleOverview from "./Articles_overview";
+import ArticleOverview from "./ArticlesOverview";
+import ForParents from "./ForParents";
+import GalleryHome from "./Gallery/index";
 const Home = () => {
   return (
     <div className="home-wrapper">
       <Carousel />
       <ArticleOverview />
+      <ForParents />
+      <GalleryHome />
     </div>
   );
 };
--- kindergarten/src/routes.js
+++ kindergarten/src/routes.js
@@ -5,6 +5,9 @@ import Home from "./Components/Home/index";
 import News from "./Components/Articles/index";
 import BigArticle from "./Components/Articles/Big_article";
 import ScrollToTop from "./Components/Utils/ScrollToTop";
+import ForParentsSection from "./Components/ForParents";
+import Gallery from "./Components/Gallery/Gallery";
+import GalleryItem from "./Components/Gallery/GalleryItem";
 
 const Routes = () => {
   return (
@@ -12,6 +15,9 @@ const Routes = () => {
       <ScrollToTop>
         <Header />
         <Switch>
+          <Route path="/galeria/:id" exact component={GalleryItem} />
+          <Route path="/galeria" component={Gallery} />
+          <Route path="/dla_rodzicow" component={ForParentsSection} />
           <Route path="/actualnosci/:id" exact component={BigArticle} />
           <Route path="/actualnosci" component={News} />
           <Route path="/" exact component={Home} />
