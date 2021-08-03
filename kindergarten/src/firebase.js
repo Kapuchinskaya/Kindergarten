7d92d8dbd0a5c61efb484eb8e16d75b0d0f47fa8
VolhaKuchynskaya <kapuchinskaya.study@gmail.com>
2021-08-03 12:48:55 +0200

    docs: add firebase to app

---
+++ kindergarten/src/firebase.js
@@ -0,0 +1,30 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/app";
+import "firebase/database";
+
+import { kindergartenDB } from "./temp/kindergarten-export";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBttZUQDkh7fF7OsTon1XCec6UnznT7j0g",
+  authDomain: "kindergarten-study.firebaseapp.com",
+  projectId: "kindergarten-study",
+  storageBucket: "kindergarten-study.appspot.com",
+  messagingSenderId: "1074721020838",
+  appId: "1:1074721020838:web:f30d566b40e46072ed4d67",
+  measurementId: "G-4RPY4TWTGQ",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+//passing data to FB
+
+const DB = firebase.firestore();
+const newsCollection = DB.collection("news");
+const usersCollection = DB.collection("users");
+
+// kindergartenDB.news.forEach((item) => {
+//   newsCollection.add(item);
+// });
+
+export { firebase, usersCollection, newsCollection };
