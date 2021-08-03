27330dbbbbf176727171e0b8bdb79fc9fdc0bf89
VolhaKuchynskaya <kapuchinskaya.study@gmail.com>
2021-08-03 14:19:50 +0200

    feat: add error and success toasts (reusable components)

---
+++ kindergarten/src/Components/Utils/Toasts.js
@@ -0,0 +1,13 @@
+import { toast } from "react-toastify";
+
+export const showErrorToast = (msg) => {
+  toast.error(msg, {
+    position: toast.POSITION.TOP_LEFT,
+  });
+};
+
+export const showSuccessToast = (msg) => {
+  toast.success(msg, {
+    position: toast.POSITION.TOP_LEFT,
+  });
+};
